c7b140dc6028777c02ce0496f82b21cf2c45936d
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2023-11-04 16:23:26 -0700

    Fix htmli for answer.

--- answers/html-injection/html-injection.js
+++ answers/html-injection/html-injection.js
@@ -1,5 +1,5 @@
 const displayName = () => {
   const inputElValue = document.getElementById("name").value;
   const nameDisplayEl = document.getElementById("name-display");
-  nameDisplayEl.innerHTML = "hi, " + inputElValue;
+  nameDisplayEl.textContent = "hi, " + inputElValue;
 };
